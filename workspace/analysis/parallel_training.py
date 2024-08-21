338f3c5063437a00b00703562decd294b826012c
HugoHakem <hugo.hakem@berkeley.edu>
2024-08-21 11:49:36 -0400

    Including loss save in training loop

--- workspace/analysis/parallel_training.py
+++ workspace/analysis/parallel_training.py
@@ -8,7 +8,7 @@ from torch.utils.data import DataLoader, DistributedSampler
 from torch.nn.parallel import DistributedDataParallel as DDP
 from tqdm import tqdm
 import os
-os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+#os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 
 from torch.nn.parallel import DistributedDataParallel as DDP
 
@@ -23,13 +23,14 @@ def cleanup():
     dist.destroy_process_group()
 
 def train(rank, world_size, model, model_param, adam_param, dataset,
-          batch_size, epochs, log_interval, fold):
+          batch_size, epochs, fold,
+          filename, child_conn):
     setup(rank, world_size)
     # Define model, loss function, and optimizer
     model = model(*model_param).to(rank)
     ddp_model = DDP(model, device_ids=[rank])
 
-    criterion = nn.CrossEntropyLoss().to(rank)
+    criterion = nn.CrossEntropyLoss(reduction='sum')
     optimizer = optim.Adam(ddp_model.parameters(), lr=adam_param["lr"], 
                            weight_decay=adam_param["weight_decay"])
 
@@ -40,7 +41,6 @@ def train(rank, world_size, model, model_param, adam_param, dataset,
     for epoch in (tqdm(range(epochs)) if rank==0 else range(epochs)):
         ddp_model.train()
         sampler.set_epoch(epoch)  # Ensure each epoch sees different data
-
         running_loss = 0.0
         for batch_idx, (data, target) in enumerate(dataloader):
             data, target = data.to(rank), target.to(rank)
@@ -51,22 +51,36 @@ def train(rank, world_size, model, model_param, adam_param, dataset,
             optimizer.step()
 
             running_loss += loss.item()
-            # if batch_idx % log_interval == 0:
-            #     print(f'Rank {rank}, Epoch [{epoch}/{epochs}], Step [{batch_idx}/{len(dataloader)}], Loss: {loss.item():.4f}')
             del data
             del target
             torch.cuda.empty_cache()
 
+        #Summing Loss across GPU 
+        running_loss = torch.tensor(running_loss, dtype=torch.float32, device=rank)
+        dist.all_reduce(running_loss, op=dist.ReduceOp.SUM)
+        running_loss_cpu = running_loss.cpu().numpy()
+        del running_loss
+        if rank == 0:
+            child_conn.send(running_loss_cpu / len(dataset))
+        
     if rank == 0:
-        torch.save(ddp_model.module.state_dict(), f"ddp_trained_model_fold_{fold}.pth")
+        torch.save(ddp_model.module.state_dict(), filename+f"{fold}.pth")
     cleanup()
 
 
 def run_train(model, model_param, adam_param, dataset,
-              batch_size, epochs, log_interval, fold):
+              batch_size, epochs, fold, filename):
     world_size = torch.cuda.device_count()
+
+    parent_conn, child_conn = mp.Pipe()
+    losses = []
+    
     mp.spawn(train,
-             args=(world_size, model, model_param, adam_param, dataset, batch_size, epochs, log_interval, fold),
+             args=(world_size, model, model_param, adam_param, dataset, batch_size, epochs, fold, filename, child_conn),
              nprocs=world_size,
              join=True)
+
+    while parent_conn.poll():
+        losses.append(parent_conn.recv())
+    return losses
 
