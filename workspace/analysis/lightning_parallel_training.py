ad4396ac3a1022fe0c19ea8b326b3d9a7160c7fe
HugoHakem <hugo.hakem@berkeley.edu>
2024-08-27 19:36:04 -0400

    Parallel training fixed in script

--- workspace/analysis/lightning_parallel_training.py
+++ workspace/analysis/lightning_parallel_training.py
@@ -28,6 +28,11 @@ class LightningModel(L.LightningModule):
         self.max_epoch = max_epoch
         self.n_class = n_class
         self.save_hyperparameters(ignore="model")
+        self.training_step_outputs = []
+        self.validation_step_outputs = []
+        self.accuracy = MulticlassAccuracy(num_classes=self.n_class, average="macro")
+        self.rocauc = MulticlassAUROC(num_classes=self.n_class, average="macro")
+        self.f1 = MulticlassF1Score(num_classes=self.n_class, average="macro")
 
     def training_step(self, batch, batch_idx):
         # training_step defines the train loop.
@@ -51,8 +56,10 @@ class LightningModel(L.LightningModule):
         output = self.model(inputs)
         loss = cross_entropy(output, target)
         self.validation_step_outputs.append((output, target))
+        
         if self.trainer.is_global_zero:
-            self.log("val_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True, 
+            self.log("val_loss", loss, on_step=True, on_epoch=True, prog_bar=True, 
+                     logger=True, 
                      rank_zero_only=True,
                      sync_dist=True) #do Ihave to use global_zero and sync dist? 
 
@@ -63,24 +70,37 @@ class LightningModel(L.LightningModule):
 
     def on_train_epoch_end(self):
         self._shared_eval(self.training_step_outputs, "train")
-        self.training_step_outputs.clear()  # free memory
+        self.training_step_outputs.clear() # free memory
+        self.training_step_outputs = []
 
     def on_validation_epoch_end(self): 
         self._shared_eval(self.validation_step_outputs, "val")
         self.validation_step_outputs.clear()  # free memory
+        self.validation_step_outputs = []
         
     def _shared_eval(self, prefix_step_outputs, prefix):
         all_preds, all_labels = map(lambda x: list(x), unzip(prefix_step_outputs))
-        all_preds, all_labels = self.all_gather(torch.vstack(all_preds)), self.all_gather(torch.hstack(all_labels))
+        (all_preds, all_labels) = (self.all_gather(torch.vstack(all_preds)).view(-1, self.n_class), 
+                                   self.all_gather(torch.hstack(all_labels)).view(-1))
+
         all_preds = nn.Softmax(dim=1)(all_preds)
-        metrics = {"acc": MulticlassAccuracy(num_classes=self.n_class, average=None), 
-                   "roc": MulticlassAUROC(num_classes=self.n_class, average=None), 
-                   "f1": MulticlassF1Score(num_classes=self.n_class, average=None)}
-        for (token, func) in list(metrics.items()):
-            self.log(prefix + "_" + token, func(all_preds, all_labels))
-                     
-        if self.current_epoch==self.max_epoch:
-            self.log(prefix + "_ConfusionMatrix", MulticlassConfusionMatrix(num_classes=self.n_class)(
-                all_preds, all_labels))
+
+        
+        if self.trainer.is_global_zero:
+            self.log(prefix + "_" + "acc", self.accuracy(all_preds, all_labels), 
+                     rank_zero_only=True,
+                     sync_dist=True)
+            self.log(prefix + "_" + "roc", self.rocauc(all_preds, all_labels), 
+                     rank_zero_only=True,
+                     sync_dist=True)
+            self.log(prefix + "_" + "f1", self.f1(all_preds, all_labels),
+                     rank_zero_only=True,
+                     sync_dist=True)
+                             
+            # if self.current_epoch==self.max_epoch-1:
+            #     self.log(prefix + "_ConfusionMatrix", 
+            #              MulticlassConfusionMatrix(num_classes=self.n_class)(all_preds, all_labels), 
+            #              rank_zero_only=True,
+            #              sync_dist=True)
 
 
