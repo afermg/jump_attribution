c9233b40f695cc14557ad0584b1eb27f9fab94e0
HugoHakem <hugo.hakem@berkeley.edu>
2024-08-29 18:32:31 -0400

    Start generative part on Diane example

--- workspace/analysis/custom_dataset.py
+++ workspace/analysis/custom_dataset.py
@@ -14,3 +14,18 @@ class ImageDataset(Dataset):
 
     def __getitem__(self, idx): 
         return self.imgs[idx,:,:], self.img_labels[idx]
+
+
+class RowDataset(Dataset):
+    def __init__(self, row, labels, transform=None, target_transform=None):
+        super().__init__()
+        self.row = row
+        self.row_labels = labels
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.row_labels)
+
+    def __getitem__(self, idx): 
+        return self.row[idx,:], self.row_labels[idx]
