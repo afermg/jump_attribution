ea8d1598df2575d25d4949d9cab76fe15ef52b28
HugoHakem <hugo.hakem@berkeley.edu>
2024-08-29 14:16:29 -0400

    Model VGG trained

--- workspace/analysis/conv_model.py
+++ workspace/analysis/conv_model.py
@@ -45,3 +45,47 @@ class VGG(nn.Module):
         return self.sequence(x)
 
 
+
+class VGG_ch(nn.Module):
+    def __init__(self, img_depth, img_size, lab_dim, conv_n_ch, n_conv_block, n_conv_list, n_lin_block, p_dropout):
+        super().__init__()
+        self.img_depth = img_depth
+        self.img_size = img_size
+        self.img_size = img_size
+        self.lab_dim = lab_dim
+        self.n_conv_block = n_conv_block
+        self.n_conv_list = n_conv_list
+        self.n_lin_block = n_lin_block
+        self.conv_n_ch = conv_n_ch
+        self.fc_dim = self.conv_n_ch * (2 ** (self.n_conv_block - 1)) * ((self.img_size // (2 ** self.n_conv_block)) ** 2)
+        self.relu = nn.ReLU()
+        self.drop = nn.Dropout(p=p_dropout)
+        self.sequence = nn.Sequential(
+                        *[self.conv_block((self.conv_n_ch * (2 ** (i-1)) if i != 0 else self.img_depth),
+                                          (self.conv_n_ch * (2 ** i) if i!=0 else self.conv_n_ch),
+                                          self.n_conv_list[i])
+                                     for i in range(self.n_conv_block)],
+                        nn.Flatten(),
+                        *[self.linear_block(self.fc_dim // (4 ** i), self.fc_dim // (4 ** (i + 1)))
+                          for i in range(self.n_lin_block - 1)],
+                        nn.Linear(self.fc_dim // (4 ** (self.n_lin_block - 1)), self.lab_dim))
+
+    def conv_block(self, in_ch, out_ch, num_conv):
+        return nn.Sequential(
+            *sum([(nn.Conv2d(in_channels=(in_ch if i==0 else out_ch), out_channels=out_ch,
+                             kernel_size=3, stride=1, padding=1),
+                   nn.BatchNorm2d(out_ch),
+                   self.relu)
+              for i in range(num_conv)], ()),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        )
+
+    def linear_block(self, in_dim, out_dim):
+        return nn.Sequential(
+            nn.Linear(in_features=in_dim, out_features=out_dim),
+            self.relu,
+            self.drop
+        )
+
+    def forward(self, x):
+        return self.sequence(x)
